c108f7eeb1a9eab49f468c5d8fe26e2627207a60
Salim-Kh <khairis24982@masseyhigh.school.nz>
2025-11-17 10:51:06 +1300

    Mass conversion

---
+++ 06_dict_mass.py
@@ -0,0 +1,23 @@
+# Mass
+Mass_dict = {
+    "mg" :1000,
+    "gr" : 1000,
+    "kg": 1,
+    "t": .001
+
+}
+# Get amounts and unit (assume they are valid)
+amount = float(input("how much? "))
+from_unit = input("from Unit? ")
+To_Unit = input("To Unit? ")
+
+
+# Multiply to get to our standard value...
+multiply_by = Mass_dict[To_Unit]
+standard = amount * multiply_by
+
+# Divide to get to our desired value
+divide_by = Mass_dict[from_unit]
+answer = standard / divide_by
+
+print(f"There are {answer} {To_Unit} in {amount} {from_unit} " )
